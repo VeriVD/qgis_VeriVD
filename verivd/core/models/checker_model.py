5be6191c3be2cd0014df92ab44d017d68ce70684
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-05-22 09:14:16 +0200

    update pre-commit

--- verivd/core/models/checker_model.py
+++ verivd/core/models/checker_model.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 /***************************************************************************
  VeriVD plugin
@@ -14,25 +13,20 @@
  ***************************************************************************/
 """
 
-from qgis.core import (
-    QgsSymbolLayer,
-    QgsProperty,
-    QgsVectorLayer,
-    QgsWkbTypes,
-)
+from qgis.core import QgsProperty, QgsSymbolLayer, QgsVectorLayer, QgsWkbTypes
 from qgis.gui import QgisInterface
 
+from verivd.core.gpkg_data import MARKER_SHAPE
 from verivd.core.layer_info import LayerInfo
 from verivd.core.layer_list_model import LayerListModel
-from verivd.core.topic_layers import TOPIC_LAYERS
-from verivd.core.gpkg_data import MARKER_SHAPE
 from verivd.core.symbology_type import SymbologyType
+from verivd.core.topic_layers import TOPIC_LAYERS
 from verivd.core.veri_meta_layer import VeriMetaLayer
 
 
 class CheckerLayerModel(LayerListModel):
     def __init__(self, iface: QgisInterface):
-        super(CheckerLayerModel, self).__init__(iface)
+        super().__init__(iface)
 
     def reload(self):
         self.beginResetModel()
@@ -42,43 +36,43 @@ class CheckerLayerModel(LayerListModel):
         checker_dict = self.gpkg_data.load_table_list("000_checker_decompte")
         for topic in TOPIC_LAYERS:
             if topic in checker_dict:
-                display_name = "Checker - {}: {}".format(topic, str(checker_dict[topic]))
+                display_name = f"Checker - {topic}: {str(checker_dict[topic])}"
                 self._veri_meta_layers.append(VeriMetaLayer(topic, display_name))
         self.endResetModel()
 
     def group_name(self, layer):
-        return "Résultat du checker - {}".format(layer)
+        return f"Résultat du checker - {layer}"
 
     def layer_infos(self, layer: str) -> [LayerInfo]:
-        sql_request = "\"topic\" = '{}'".format(layer)
+        sql_request = f"\"topic\" = '{layer}'"
         layer_infos = (
             LayerInfo(
-                display_name="Justificatifs - {} point".format(layer),
+                display_name=f"Justificatifs - {layer} point",
                 layer_name="justificatif_point",
                 sql_request=f"\"layer\" = '000_checker_point' AND {sql_request}",
                 symbology_type=SymbologyType.QML,
             ),
             LayerInfo(
-                display_name="Justificatifs - {} ligne".format(layer),
+                display_name=f"Justificatifs - {layer} ligne",
                 layer_name="justificatif_line",
                 # les lignes sont dans la couche _surface
                 sql_request=f"\"layer\" = '000_checker_surface' AND {sql_request}",
                 symbology_type=SymbologyType.QML,
             ),
             LayerInfo(
-                display_name="Justificatifs - {} surface".format(layer),
+                display_name=f"Justificatifs - {layer} surface",
                 layer_name="justificatif_polygon",
                 sql_request=f"\"layer\" = '000_checker_surface' AND {sql_request}",
                 symbology_type=SymbologyType.QML,
             ),
             LayerInfo(
-                display_name="Justificatifs - {} sans géométrie".format(layer),
+                display_name=f"Justificatifs - {layer} sans géométrie",
                 layer_name="justificatif_nogeometry",
                 sql_request=f"\"layer\" = '000_checker_sans_geometrie' AND {sql_request}",
                 symbology_type=SymbologyType.NO_SYMBOL,
             ),
             LayerInfo(
-                display_name="Checker - {} point".format(layer),
+                display_name=f"Checker - {layer} point",
                 layer_name="000_Checker_Point",
                 sql_request=sql_request,
                 symbology_type=SymbologyType.RANDOM_CATEGORIZED,
@@ -86,7 +80,7 @@ class CheckerLayerModel(LayerListModel):
                 symbology_data_defined_properties={QgsSymbolLayer.PropertySize: QgsProperty.fromValue(5)},
             ),
             LayerInfo(
-                display_name="Checker - {} surface".format(layer),
+                display_name=f"Checker - {layer} surface",
                 layer_name="000_Checker_Surface",
                 sql_request=sql_request,
                 symbology_type=SymbologyType.RANDOM_CATEGORIZED,
@@ -95,7 +89,7 @@ class CheckerLayerModel(LayerListModel):
                 opacity=0.5,
             ),
             LayerInfo(
-                display_name="Checker - {} sans géométrie".format(layer),
+                display_name=f"Checker - {layer} sans géométrie",
                 layer_name="000_Checker_Sans_geometrie",
                 sql_request=sql_request,
                 symbology_type=SymbologyType.NO_SYMBOL,
